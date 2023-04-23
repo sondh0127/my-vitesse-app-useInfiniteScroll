a2fc80daad64ac90c51c8f908ec9438529a7164e
Son Hong Do <sondh0127@gmail.com>
2023-04-23 22:22:05 +0700

    init

---
+++ src/layouts/home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const el = ref<HTMLElement | null>(null)
+const data = ref([1, 2, 3, 4, 5, 6])
+
+useInfiniteScroll(
+  el,
+  () => {
+    const length = data.value.length + 1
+    data.value.push(...Array.from({ length: 5 }, (_, i) => length + i))
+  },
+  { distance: 10 },
+)
+</script>
+
+<template>
+  <div ref="el" class="m-auto h-300px w-300px flex flex-col gap-2 overflow-y-scroll rounded bg-gray-500/5 p-4">
+    <div v-for="item in data" :key="item" class="h-30 rounded bg-gray-500/5 p-3">
+      {{ item }}
+    </div>
+  </div>
+</template>
